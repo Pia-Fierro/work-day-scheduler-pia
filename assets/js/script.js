d5cc76aaef296a0cf76decd19db850cef45c714a
pia fierro <piafierro.3187@gmail.com>
2022-12-15 21:33:15 +1000

    Fixed local storage fuction. Render local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,44 @@
 
-var currentDay = dayjs();
-var existingTask = [];
-
-
   
 // Show current day on header :format Do to show date ordinality not working
-$('#currentDay').text(currentDay.format('dddd, MMM Do'))
+var currentDay = dayjs();
+var schedule = $(this).parent().children(".schedule").val();
+
+function init () {
+
 
 //current time frame and hour
 
 
 //compare current time and time of task:add class past, present and future to time block 
-//comaparing the hour in the ID to the current hour
 
 
-//current task is save in local storage when click save button 
+// render saved schedule task present in local storage if it exist 
+$('.time-block').each(function () {
+  var id = $(this).attr("id");
+  var schedule = localStorage.getItem(id);
+  if (schedule !== null) {
+    $(this).children(".description").val(schedule);
+  }
+    else {
+      $(this).children(".description").val()
+    }
+  
+  }
+);
+
+
+//time and schedule task is save in local storage when clicking save button 
+
 $('.saveBtn').click(function(){
-  currentTask ={
-    time : $(this).parent().attr('id'),
-    schedule : $(this).parent().children('textarea').val(),
-  };
-  if (currentTask.task !== null) {
-    existingTask.push(currentTask)
-    localStorage.setItem('saved task',JSON.stringify(existingTask));
+    var time = $(this).parent().attr('id');
+    var schedule = $(this).parent().children(".description").val();
+    localStorage.setItem(time, schedule);
   } 
-})
+)
 
+//code to display current date in top of website  
+$('#currentDay').text(currentDay.format('dddd, MMM Do'))
 
-
+}
+init();
